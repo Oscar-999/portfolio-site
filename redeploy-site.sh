e9ea764ebd0d0aa3eaa3fbcf6da66864f771c3fd
Oscar <oscarvac74@gmail.com>
2024-07-25 01:02:44 -0500

    redeploy

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,9 +6,8 @@ cd ./portfolio-site
 # Fetch latest changes from GitHub main branch and reset local repository
 git fetch && git reset origin/main --hard
 
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
+# Spin down existing containers to avoid out-of-memory issues
+docker compose -f docker-compose.prod.yml down
 
-pip install -r requirements.txt
-
-systemctl restart myportfolio
+# Rebuild and start the containers in detached mode
+docker compose -f docker-compose.prod.yml up -d --build
